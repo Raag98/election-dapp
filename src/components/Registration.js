69e1afe4d57e3e8ff188c3af7e9400cb1c2e5a5b
rjrealworld <riyajn2001@gmail.com>
2022-05-09 00:05:50 +0530

    Updated registration component css

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,82 +1,114 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
-import db from '../firebase/firebase';
-import { collection, doc, updateDoc } from 'firebase/firestore';
+import { Navigate, Link } from "react-router-dom";
+import { collection, doc, updateDoc } from "firebase/firestore";
+import db from "../firebase/firebase";
 
-export default function Registration() {
-    const [phase, setPhase] = useState("registration");
-    const [name, setName] = useState();
-    const [aadhar, setAadhar] = useState();
-    const [wallet, setWallet] = useState();
-    const [approved, setApproved] = useState(false);
+import "./Registration.css";
 
-    const { currentUser } = useAuth();
+const Registration = () => {
+  const [phase, setPhase] = useState("registration");
+  const [name, setName] = useState();
+  const [aadhar, setAadhar] = useState();
+  const [wallet, setWallet] = useState();
+  const [approved, setApproved] = useState(false);
 
-    const setValues = async () => {
-      // Update values in Firebase
-      
-      const docRef = doc(db, "voters", currentUser.uid);
-      
-      await updateDoc(docRef, {
-        aadhar: aadhar,
-        walletAddr: wallet,
-      });
-      
-      console.log("Update Values!");
-    }
+  const { currentUser } = useAuth();
 
+  const setValues = async () => {
+    // Update values in Firebase
 
-    useEffect(() => {
-      setName(currentUser.displayName);      
-    }, []);
+    const docRef = doc(db, "voters", currentUser.uid);
 
+    await updateDoc(docRef, {
+      aadhar: aadhar,
+      walletAddr: wallet,
+    });
 
-    return (
-      <div className="registration">
-        <h1>Register Yourself!</h1>
-        {phase === "registration" ? (
-          <div className="reg-form">
-            <label>Enter your Aadhar</label>
-            <input
-              type="text"
-              value={aadhar}
-              onChange={(e) => setAadhar(e.target.value)}
-            />
+    console.log("Update Values!");
+  };
 
-            <label>Enter your MetaMask Wallet Address</label>
-            <input
-              type="password"
-              value={wallet}
-              onChange={(e) => setWallet(e.target.value)}
-            />
-            <button onClick={setValues}>Proceed</button>
-          </div>
-        ) : (
-          <h2>Registrations Phase Over! You can't register now.</h2>
-        )}
+  useEffect(() => {
+    setName(currentUser.displayName);
+  }, []);
 
-        <table id="voters">
-          <tr>
-            <th>Name</th>
-            <th>Aadhar</th>
-            <th>Wallet Address</th>
-            <th>Registration Status</th>
-          </tr>
-          <tr>
-            <td>{name}</td>
-            <td>{aadhar}</td>
-            <td>{wallet}</td>
-            {approved ? (
-              <td>
-                <button className="reg-btn">Registered</button>
-              </td>
-            ) : (
-              <td>
-                <button className="unreg-btn">Unregistered</button>
-              </td>
-            )}
-          </tr>
-        </table>
+  return (
+    <div className="Registration">
+      <div className="registrationForm">
+        <div className="registrationFormCenter">
+          <p className="registrationFormTitle">Register Yourself</p>
+          {phase === "registration" ? (
+            <form className="registrationFormFields">
+              <div className="registrationFormField">
+                <label className="registrationFormFieldLabel" htmlFor="aadhaar">
+                  Enter your Aadhaar
+                </label>
+                <input
+                  type="text"
+                  id="aadhaar"
+                  className="registrationFormFieldInput"
+                  placeholder="Enter aadhaar number"
+                  value={aadhar}
+                  onChange={(e) => setAadhar(e.target.value)}
+                />
+              </div>
+
+              <div className="registrationFormField">
+                <label
+                  className="registrationFormFieldLabel"
+                  htmlFor="metamask"
+                >
+                  Enter your MetaMask Wallet Address
+                </label>
+                <input
+                  type="password"
+                  id="metamask"
+                  className="registrationFormFieldInput"
+                  placeholder="Enter candidate party"
+                  value={wallet}
+                  onChange={(e) => setWallet(e.target.value)}
+                />
+              </div>
+
+              <div className="registrationFormField">
+                <button
+                  className="registrationFormFieldButton"
+                  onClick={setValues}
+                >
+                  Proceed
+                </button>
+              </div>
+            </form>
+          ) : (
+            <h2>Registrations Phase Over! You can't register now.</h2>
+          )}
+
+          <table id="voters">
+            <tr>
+              <th>Name</th>
+              <th>Aadhar</th>
+              <th>Wallet Address</th>
+              <th>Registration Status</th>
+            </tr>
+            <tr>
+              <td>{name}</td>
+              <td>{aadhar}</td>
+              <td>{wallet}</td>
+              {approved ? (
+                <td>
+                  <button className="reg-btn">Registered</button>
+                </td>
+              ) : (
+                <td>
+                  <button className="unreg-btn">Unregistered</button>
+                </td>
+              )}
+            </tr>
+          </table>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
+
+export default Registration;
