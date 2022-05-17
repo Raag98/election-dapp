684a401129a98febe6d829274b2d16bbb83e9bce
rjrealworld <riyajn2001@gmail.com>
2022-05-18 01:12:58 +0530

    Added css to result section

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getDoc, doc, collection } from 'firebase/firestore';
 import db from '../firebase/firebase';
-
+import './Results.css';
 const ResultsRow = ({name, party, photo, votes}) => {
 
   return (
